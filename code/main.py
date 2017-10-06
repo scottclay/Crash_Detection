e2930a93f450e02de8c4189641158c365f467c53
scottclay <scottclay8@gmail.com>
2017-10-06 01:28:07 +0100

    new line of code to remove duplicate entiries in final events dataframe

--- code/main.py
+++ code/main.py
@@ -16,15 +16,7 @@ from functions import Plot_crash
 from functions import Calc_distance
 
 dfs, sorted_dfs, rolling_mean, rolling_std = fetch_data(datapath,'1s')
-                
-#event = Find_events(sorted_dfs, rolling_mean, rolling_std,'mag','1s')
-
-#event = Gather_info(event, sorted_dfs, 30)
-
-
-#for i in range(0,len(event.index)):
-#    Plot_crash(i,event.iloc[i:i+1],sorted_dfs[event.driver[i]], rolling_mean[event.driver[i]], rolling_std[event.driver[i]])
-    
+                    
     
 new_dfs = Calc_distance(dfs)
 sorted_dfs, rolling_mean, rolling_std = prepare_dfs(dfs,'1s')
@@ -36,6 +28,9 @@ event = Gather_info(event, sorted_dfs, gps_dfs, 30)
 
 
 event_cut = event[(event.speed_before>event.speed_after) & (event.distance_after<10) & ((event.bearing_after - event.bearing_before)>100)]
+
+event_cut = event_cut.drop_duplicates(subset=['driver'],keep='first')
+
 for i in range(0,len(event_cut.index)):
     Plot_crash(i, event_cut.iloc[i:i+1],sorted_dfs[event_cut.driver[i]], rolling_mean[event_cut.driver[i]], gps_dfs[event_cut.driver[i]])
 
