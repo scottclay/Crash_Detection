71cc04b2ede5f254a9079fcc17502f1f4cd1681e
scottclay <scottclay8@gmail.com>
2017-10-08 17:33:10 +0100

    added print statements to see progress

--- code/main.py
+++ code/main.py
@@ -23,16 +23,20 @@ datapath = os.path.join(dir, '../data/')
 output_path = os.path.join(dir, '../output/')
 
 # Read data/Find events
+print("Reading in and sorting data")
 files, dfs, sorted_dfs, rolling_mean, rolling_std = fetch_data(datapath,time_window)    
 new_dfs = calc_distance(dfs)
 sorted_dfs, rolling_mean, rolling_std = prepare_dfs(dfs,time_window)
 gps_dfs = calc_distance(sorted_dfs)
+print("Finding potential events")
 event = find_events(files, sorted_dfs, rolling_mean, rolling_std, test_variable, time_window, sigma)
 event = gather_info(event, sorted_dfs, gps_dfs, 30)
 
 # Cut the data based on a selection criteria to find crash events
+print("Cutting the selection")
 event_cut = event[(event.speed_before>event.speed_after) & (event.distance_after<10) & ((event.bearing_after - event.bearing_before)>100)]
-event_cut = event_cut.drop_duplicates(subset=['driver'],keep='first')
+event_cut = event_cut.drop_duplicates(subset=['driver'],keep='first') #only keep first event for each journey 
+print("Number of potential crash events:", len(event_cut.index))
 
 #Produce outputs - plots for each event, and then text output
 text_file = open(output_path+"output.txt", "w")
@@ -41,9 +45,7 @@ print("File name \t Time of event \t Acceleration \n")
 for i in range(0,len(event_cut.index)):
     plot_crash(output_path, i, event_cut.iloc[i:i+1],event_cut.filename[i],sorted_dfs[event_cut.driver[i]], rolling_mean[event_cut.driver[i]], gps_dfs[event_cut.driver[i]])
     print(event_cut.filename[i], '\t', str(event_cut.index[i]), '\t', event_cut.mag[i])
-    #text_file.write(event_cut.filename[i], '\t', str(event_cut.index[i]), '\t', event_cut.mag[i])
     text_file.write("%s \t %s \t %f \n" % (event_cut.filename[i], str(event_cut.index[i]), event_cut.mag[i]))
-
 
 text_file.close()
 
